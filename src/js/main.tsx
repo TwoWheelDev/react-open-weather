88f9c612ee25592f00b6b3e20e9502e4f1dee4cb
Daniel Lintott <git@dlintott.dev>
2025-12-23 23:39:13 +0000

    Switch to Vite dev server

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -8,14 +8,14 @@ import ReactWeather, {
 } from '.';
 
 const App = () => {
-  //   const { data, isLoading, errorMessage } = useOpenMeteo({
-  //   // key: 'YOUR-API-KEY',
-  //   // prefix: 'YOUR-CUSTOMER-PREFIX',
-  //   lat: '52.583698',
-  //   lon: '0.056963',
-  //   lang: 'en',
-  //   // unit: { temperature: 'fahrenheit', wind_speed: 'mph' },
-  // });
+    const { data, isLoading, errorMessage } = useOpenMeteo({
+    // key: 'YOUR-API-KEY',
+    // prefix: 'YOUR-CUSTOMER-PREFIX',
+    lat: '52.583698',
+    lon: '0.056963',
+    lang: 'en',
+    // unit: { temperature: 'fahrenheit', wind_speed: 'mph' },
+  });
 
   // const { data, isLoading, errorMessage } = useVisualCrossing({
   //   key: 'YOUR_API_KEY',
@@ -25,13 +25,13 @@ const App = () => {
   //   unit: 'metric',
   // });
 
-  const { data, isLoading, errorMessage } = useOpenWeather({
-    key: 'YOUR_API_KEY',
-    lat: '48.137154',
-    lon: '11.576124',
-    lang: 'en',
-    unit: 'metric',
-  });
+  // const { data, isLoading, errorMessage } = useOpenWeather({
+  //   key: 'YOUR_API_KEY',
+  //   lat: '48.137154',
+  //   lon: '11.576124',
+  //   lang: 'en',
+  //   unit: 'metric',
+  // });
   // const { data, isLoading, errorMessage } = useWeatherBit({
   //   lat: '48.137154',
   //   lon: '11.576124',
