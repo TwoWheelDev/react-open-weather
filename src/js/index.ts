9ad8264c7f53508441a85fcf1ac30d1e56bd926c
Daniel Lintott <git@dlintott.dev>
2025-12-23 00:53:22 +0000

    Rename src/js/index.js to .ts

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,9 @@
 import ReactWeather from './components/ReactWeather';
 
-export default ReactWeather;
+export type { ReactWeatherProps, WeatherData } from './components/ReactWeather';
 export { default as useOpenWeather } from './providers/openweather/useOpenWeather';
 export { default as useWeatherBit } from './providers/weatherbit/useWeatherBit';
 export { default as useVisualCrossing } from './providers/visualcrossing/useVisualCrossing';
 export { default as useOpenMeteo } from './providers/open-meteo/useOpenMeteo';
+
+export default ReactWeather
